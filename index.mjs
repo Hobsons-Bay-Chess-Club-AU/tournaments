52e33e05bef9ad33a8aaea72569b9319572e8d62
Truong Nguyen <samuraitruong@hotmail.com>
2024-02-28 09:04:49 +1100

    update rewards

--- index.mjs
+++ index.mjs
@@ -141,9 +141,8 @@ function ranking() {
 
   const players = readPlayerList();
   const { title, subTitle, standings: standingList } = readStanding();
-  console.log(players);
+  // console.log(players);
   for (const standing of standingList) {
-    console.log(standing);
     const p = players.find((x) => x.N === standing.N);
     standing.U = p.U;
     standing.GROUP = p.U;
@@ -154,6 +153,7 @@ function ranking() {
     standing.BHC1 = standing["BH-C1"];
   }
   standingList[0].GROUP = "Open";
+
   const rewardList = ["Open", "Girl", "U12", "U10", "U8"];
   // console.log(standingList);
   const reward = ["🏆 Winner", "🥈 1st runner up", "🥉 2nd runner up"];
@@ -167,7 +167,66 @@ function ranking() {
     {}
   );
 
-  console.log(rewards);
+  // Adjustment the reward outside category
+  // find the player in the category that not have prize but hight point than other player
+  const adjustment = ["U8", "U10", "U12", "Open"];
+
+  let catIndex = 1;
+
+  for (const category of adjustment) {
+    var currentCat = rewards.find((x) => x.category === category);
+    const list = currentCat.data;
+
+    const upperCategories = adjustment.slice(catIndex);
+    console.log("category ", category, upperCategories);
+
+    for (var index = 3; index <= list.length; index++) {
+      const playerStanding = list[index];
+      if (playerStanding && upperCategories.length > 0) {
+        //  console.log(playerStanding);
+        let swapped = false;
+        for (const upercat of upperCategories) {
+          var cat = rewards.find((x) => x.category === upercat);
+          const upperCatList = cat.data;
+          if (swapped) {
+            break;
+          }
+          console.log(upperCatList);
+
+          for (let rIndex = 0; rIndex < 3; rIndex++) {
+            console.log("compare", playerStanding, upperCatList[rIndex]);
+            if (
+              +playerStanding.Pts > +upperCatList[rIndex].Pts ||
+              (playerStanding.Pts == upperCatList[rIndex].Pts &&
+                +playerStanding.BH > +upperCatList[rIndex].BH)
+            ) {
+              cat.data = [
+                ...cat.data.slice(0, rIndex),
+                playerStanding,
+                ...cat.data.slice(rIndex),
+              ];
+
+              // remove player form his current category
+              currentCat.data = currentCat.data.filter(
+                (x) => x.NAME !== playerStanding.NAME
+              );
+              // update index & prizes
+              cat.data = cat.data.map((x, index) => ({
+                ...x,
+                Pos: index + 1,
+                Reward: reward[index],
+              }));
+              swapped = true;
+              break;
+            }
+          }
+        }
+      }
+    }
+    catIndex++;
+  }
+
+  // re-order the list
   const template = Handlebars.compile(raw);
 
   const html = template({
