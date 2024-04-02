91261b6b0806fe834d8ca82bcb31d372c85afda4
FTP Sync <ftp@hbcc.com>
2024-04-02 20:42:48 +0000

    Sync FTP changes

--- www/wwwC.J.SPurdyCupJuniors2024/pairing.js
+++ www/wwwC.J.SPurdyCupJuniors2024/pairing.js
@@ -1,4 +1,7 @@
-document.write ('     <nav class="me-0 me-lg-3" aria-label="Page navigation">          <ul class="pagination mb-lg-0">    <li class="page-item"><a class="page-link" href="pairs1.html">1</a></li>    <li class="page-item"><a class="page-link" href="pairs2.html">2</a></li>     </ul>  </nav> ')
-item base-hover"><a class="page-link" href="pairs3.html">3</a></li>
+<nav class="me-0 me-lg-3" aria-label="Page navigation">
+          <ul class="pagination mb-lg-0">
+    <li class="page-item base-hover"><a class="page-link" href="pairs1.html">1</a></li>
+    <li class="page-item base-hover"><a class="page-link" href="pairs2.html">2</a></li>
+    <li class="page-item base-hover"><a class="page-link" href="pairs3.html">3</a></li>
      </ul>
   </nav>
