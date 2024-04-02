f16778470e49c9ebb5ddd65a83ebc2def7776b50
Truong Nguyen <samuraitruong@hotmail.com>
2024-04-03 08:17:08 +1100

    disabled html sync

--- update-html.mjs
+++ update-html.mjs
@@ -1,9 +1,38 @@
 import fs from 'fs';
 import path from 'path';
 import axios from 'axios';
+import puppeteer from 'puppeteer';
 
 const rootDir = './www';
 
+async function getCookieFromUrl(url) {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+
+    await page.goto(url, { waitUntil: 'networkidle2' });
+
+    await page.waitForFunction(() => {
+        return document.cookie.includes('test');
+    });
+
+    const cookies = await page.cookies();
+    let targetCookie = null;
+
+    for (const cookie of cookies) {
+        if (cookie.name === 'test') { // Replace 'yourCookieName' with the actual name of the cookie you want to check
+            targetCookie = cookie;
+            break;
+        }
+    }
+
+    await browser.close();
+
+    return targetCookie;
+}
+
+async function getCookie() {
+    return "__test=1670aca879a4d45aedc2e780e9ca765d"
+}
 async function fetchAndWriteHtml(filePath , retry=1) {
     console.log("filePath", filePath)
 
@@ -27,11 +56,17 @@ async function fetchAndWriteHtml(filePath , retry=1) {
         const htmlFilePath = filePath.replace('.php', '.html');
         if(htmlContent.includes("This site requires Javascript to work")) 
         {
+            if(retry < 3) {
+                headers.Cookie = await getCookie();
+
+                await fetchAndWriteHtml(filePath, retry++)
+                }
+            else
             console.warn("Invalid coookies, stop overwritten file")
-            return
+
         }
         fs.writeFileSync(htmlFilePath, htmlContent);
-        console.log(`Successfully fetched and saved: ${htmlFilePath}`);
+        console.log(`Successful mirror ${pathUrl} => ${htmlFilePath}`);
     } catch (error) {
         console.error(`Error fetching or writing ${filePath}:`, error);
         if(error.response.status === 508 && retry<100) {
