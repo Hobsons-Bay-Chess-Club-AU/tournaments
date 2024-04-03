cf7764dd03c6c5f9741f64863e483e992074124f
Truong Nguyen <samuraitruong@hotmail.com>
2024-04-03 21:18:25 +1100

    remove old file

--- www/wwwc.j.s purdy cup juniors 2024/game.js
+++
@@ -1,1 +0,0 @@
-document.write ('<h4>Download PGN:</h4> <div class="btn-toolbar"> <div class="btn-group"><a class="btn btn-default btn-sm" href=\"/c.j.s purdy cup juniors 20241.pgn\" > 1 </a> <a class="btn btn-default btn-sm" href=\"/c.j.s purdy cup juniors 20242.pgn\" > 2 </a> </div> </div> ')
