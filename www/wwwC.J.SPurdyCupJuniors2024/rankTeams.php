5de10b10fd9961270154985066499a73e58aa207
FTP Sync <ftp@hbcc.com>
2024-04-18 04:29:39 +0000

    Sync FTP changes

--- www/wwwC.J.SPurdyCupJuniors2024/rankTeams.php
+++ www/wwwC.J.SPurdyCupJuniors2024/rankTeams.php
@@ -11,7 +11,7 @@
     </head>
 <body>
 <div class="d-flex flex-column justify-content-between h-100">
- <nav class="navbar navbar-expand-lg fixed-top">
+ <nav class="navbar navbar-expand-lg fixed-top" data-bs-theme="dark">
                 <div class="container">    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span class="navbar-toggler-icon"></span>
                     </button>
@@ -23,7 +23,7 @@
 <ul class="dropdown-menu"><li><a class="dropdown-item" href="index.php">Sorted by Rating</a></li><li><a class="dropdown-item" href="playersname.php">Sorted by Name</a></li></ul></li><li class="nav-item"><a class="nav-link " href="pairs4.php" >Pairings</a>
 </li><li class="nav-item"><a class="nav-link" href="standings.php" >Standings</a>
 </li><li class="nav-item dropdown">   <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">Tables</a>
-<ul class="dropdown-menu"><li><a class="dropdown-item" href="crosstable.php"> Cross table </a></li><li><a class="dropdown-item" href="crosstablescore.php"> Cross table and standings </a></li><li><a class="dropdown-item" href="rankinggroup.php"> Standings by Rtg groups </a></li><li><a class="dropdown-item" href="rankingagegroup.php"> Standings by age groups </a></li><li><a class="dropdown-item" href="rankcat.php"> Standings by title </a></li><li><a class="dropdown-item" href="rankTeams.php"> Rank Teams </a></li><li><a class="dropdown-item" href="summaryFederations.php"> Summary Federations </a></li><li><a class="dropdown-item" href="summaryOrigin.php"> Summary Origin </a></li></ul></li><li class="nav-item dropdown">   <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">Reports</a>
+<ul class="dropdown-menu"><li><a class="dropdown-item" href="crosstable.php"> Cross table </a></li><li><a class="dropdown-item" href="rankinggroup.php"> Standings by Rtg groups </a></li><li><a class="dropdown-item" href="rankingagegroup.php"> Standings by age groups </a></li><li><a class="dropdown-item" href="rankcat.php"> Standings by title </a></li><li><a class="dropdown-item" href="rankTeams.php"> Rank Teams </a></li><li><a class="dropdown-item" href="summaryFederations.php"> Summary Federations </a></li></ul></li><li class="nav-item dropdown">   <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">Reports</a>
 <ul class="dropdown-menu">          <li><a class="dropdown-item" href="felovar.php">FIDE Rating</a></li>          <li><a class="dropdown-item" href="fideplayercard.php">FIDE Player Card</a></li>           <li><a class="dropdown-item" href="nelovar.php">National rating</a></li>          <li><a class="dropdown-item" href="playercard.php">National Player Card</a></li></ul></li></ul> </div>
 </div>
 </nav>
@@ -45,53 +45,53 @@
 <div class="table-responsive"><table class="table table-striped"> 
 <thead><tr><th> Pos </th> <th> Team Name </th><th>Points</th></tr></thead> <tbody>
 <tr>
-<td>1</td> <td></td> <td>12</td> </tr>
+<td>1</td> <td></td> <td>14</td> </tr>
 </tbody>
 </table>
 <h5 class="mb-4 mt-4"> Team composition</h5>
 <table class="table table-striped "> 
-<thead><tr><th>1</th> <th> </th><th>12</th></tr></thead><tbody> 
+<thead><tr><th>1</th> <th> </th><th>14</th></tr></thead><tbody> 
+<tr> <td>1</td> <td>Nguyen,Anh Kiet </td><td>4</td></tr> 
+<tr> <td>10</td> <td>Dhar,Shubham </td><td>4</td></tr> 
+<tr> <td>2</td> <td>Wei,Andrew </td><td>3</td></tr> 
 <tr> <td>5</td> <td>Batyrbekov,Kaisar </td><td>3</td></tr> 
 <tr> <td>8</td> <td>Huang,Michael </td><td>3</td></tr> 
-<tr> <td>1</td> <td>Nguyen,Anh Kiet </td><td>3</td></tr> 
-<tr> <td>10</td> <td>Dhar,Shubham </td><td>3</td></tr> 
+<tr> <td>19</td> <td>Ashok,Arjun </td><td>3</td></tr> 
+<tr> <td>3</td> <td>Majety,Rohan </td><td>3</td></tr> 
+<tr> <td>6</td> <td>Nguyen,Tuan Kiet </td><td>3</td></tr> 
+<tr> <td>7</td> <td>Ruaya,Rohan </td><td>2.5</td></tr> 
+<tr> <td>17</td> <td>Husain,Lamiah </td><td>2.5</td></tr> 
 <tr> <td>29</td> <td>Yang,Yifan </td><td>2.5</td></tr> 
-<tr> <td>2</td> <td>Wei,Andrew </td><td>2</td></tr> 
-<tr> <td>19</td> <td>Ashok,Arjun </td><td>2</td></tr> 
-<tr> <td>3</td> <td>Majety,Rohan </td><td>2</td></tr> 
-<tr> <td>7</td> <td>Ruaya,Rohan </td><td>2</td></tr> 
+<tr> <td>11</td> <td>Goh,Nicholas </td><td>2</td></tr> 
 <tr> <td>12</td> <td>Batyrbekov,Aidan </td><td>2</td></tr> 
-<tr> <td>6</td> <td>Nguyen,Tuan Kiet </td><td>2</td></tr> 
-<tr> <td>17</td> <td>Husain,Lamiah </td><td>2</td></tr> 
+<tr> <td>22</td> <td>Jagannathan,Shashvath </td><td>2</td></tr> 
 <tr> <td>25</td> <td>Shenbaga,Kabilan </td><td>2</td></tr> 
+<tr> <td>16</td> <td>Venkatesh,Arvind </td><td>2</td></tr> 
+<tr> <td>4</td> <td>Morris,Elijah </td><td>2</td></tr> 
+<tr> <td>14</td> <td>Pathiyassery,Emir </td><td>2</td></tr> 
 <tr> <td>27</td> <td>Vassallo,Romeo </td><td>1.5</td></tr> 
-<tr> <td>11</td> <td>Goh,Nicholas </td><td>1</td></tr> 
 <tr> <td>24</td> <td>Naidu,Aarush </td><td>1</td></tr> 
 <tr> <td>13</td> <td>Guggilla,Krithvik </td><td>1</td></tr> 
 <tr> <td>28</td> <td>Vijayakumar,Rithvik </td><td>1</td></tr> 
-<tr> <td>16</td> <td>Venkatesh,Arvind </td><td>1</td></tr> 
 <tr> <td>15</td> <td>Chan,Abby </td><td>1</td></tr> 
-<tr> <td>22</td> <td>Jagannathan,Shashvath </td><td>1</td></tr> 
-<tr> <td>4</td> <td>Morris,Elijah </td><td>1</td></tr> 
-<tr> <td>18</td> <td>Amutha Raja,Nalan </td><td>1</td></tr> 
+<tr> <td>21</td> <td>Bruno,Iris </td><td>1</td></tr> 
 <tr> <td>9</td> <td>Marasinghe,Claire </td><td>1</td></tr> 
-<tr> <td>14</td> <td>Pathiyassery,Emir </td><td>1</td></tr> 
+<tr> <td>18</td> <td>Amutha Raja,Nalan </td><td>1</td></tr> 
+<tr> <td>26</td> <td>Singh,Abhyuday </td><td>1</td></tr> 
 <tr> <td>20</td> <td>Babu,Dilesh </td><td>0.5</td></tr> 
-<tr> <td>21</td> <td>Bruno,Iris </td><td>0</td></tr> 
-<tr> <td>26</td> <td>Singh,Abhyuday </td><td>0</td></tr> 
 <tr> <td>23</td> <td>Kukdeja,Aarav </td><td>0</td></tr> 
 </tbody>
 </table>
 <h5 class="mb-4 mt-4"> Best boards</h5>
 <table class="table table-striped "> 
 <thead><tr><th>Board 0</th><th> Pos </th><th> Player </th><th> Team </th><th> Points </th></tr></thead> <tbody>
-<tr> <td></td> <td>1 </td><td>Batyrbekov,Kaisar </td><td> </td><td>3</td></tr> 
+<tr> <td></td> <td>1 </td><td>Nguyen,Anh Kiet </td><td> </td><td>4</td></tr> 
 <thead><tr><th>Board 1</th><th> Pos </th><th> Player </th><th> Team </th><th> Points </th></tr></thead> <tbody>
-<tr> <td></td> <td>1 </td><td>Huang,Michael </td><td> </td><td>3</td></tr> 
+<tr> <td></td> <td>1 </td><td>Dhar,Shubham </td><td> </td><td>4</td></tr> 
 <thead><tr><th>Board 2</th><th> Pos </th><th> Player </th><th> Team </th><th> Points </th></tr></thead> <tbody>
-<tr> <td></td> <td>1 </td><td>Nguyen,Anh Kiet </td><td> </td><td>3</td></tr> 
+<tr> <td></td> <td>1 </td><td>Wei,Andrew </td><td> </td><td>3</td></tr> 
 <thead><tr><th>Board 3</th><th> Pos </th><th> Player </th><th> Team </th><th> Points </th></tr></thead> <tbody>
-<tr> <td></td> <td>1 </td><td>Dhar,Shubham </td><td> </td><td>3</td></tr> 
+<tr> <td></td> <td>1 </td><td>Batyrbekov,Kaisar </td><td> </td><td>3</td></tr> 
 </tbody>
 </table></div>
 		  </div>
@@ -103,9 +103,9 @@
             <div class="container">
                 <div class="row">
                     <div class="col-12">
-                        <div class="d-flex flex-column align-items-center text-center text-dark"><p><a href="http://www.vegachess.com" target="_blank">Generated by Vega</a> All rights reserved</p><p>(Arbiter/Club : Casey Goh - AUS)
+                        <div class="d-flex flex-column align-items-center text-center text-dark"><p><a href="http://www.vegachess.com" target="_blank">Generated by Vega</a> All rights reserved</p><p>(Arbiter/Club : Hobsons Bay Chess Club - AUS)
   <?php if (file_exists($_SERVER['DOCUMENT_ROOT'].'/siteprivacy.php')) include($_SERVER['DOCUMENT_ROOT'].'/siteprivacy.php'); ?>    </div>  </div>  </div> </div> 
    </footer>
      </div>
        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script></body>
-</html>>
+</html>
