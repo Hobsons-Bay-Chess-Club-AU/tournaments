ff64ee99f504049570a643eefa47a58ee0ef04b0
Truong Nguyen <samuraitruong@hotmail.com>
2024-06-05 22:28:50 +1000

    v

--- src/vault.mjs
+++ src/vault.mjs
@@ -9,10 +9,10 @@ function encrypt(text, password) {
   const iv = randomBytes(16);
   const key = createHash('sha256').update(password).digest();
   const cipher = createCipheriv('aes-256-cbc', key, iv);
-  
+
   let encrypted = cipher.update(text, 'utf8', 'hex');
   encrypted += cipher.final('hex');
-  
+
   return `${iv.toString('hex')}\n${salt.toString('hex')}\n${encrypted}`;
 }
 
@@ -22,10 +22,10 @@ function decrypt(encrypted, password) {
   const salt = Buffer.from(textParts[1], 'hex');
   const key = createHash('sha256').update(password).digest();
   const decipher = createDecipheriv('aes-256-cbc', key, iv);
-  
+
   let decrypted = decipher.update(textParts[2], 'hex', 'utf8');
   decrypted += decipher.final('utf8');
-  
+
   return decrypted;
 }
 
@@ -37,7 +37,7 @@ async function processFiles(folder, mode, password) {
     for (const file of files) {
       const filePath = join(folder, file);
       const fileStat = await stat(filePath);
-      
+
       if (fileStat.isDirectory()) {
         await processFiles(filePath, mode, password); // Recursive call for sub-folder
       } else {
@@ -47,8 +47,8 @@ async function processFiles(folder, mode, password) {
         if (mode === 'encrypt' && fileExt === '.csv') {
           const fileContent = await readFile(filePath, 'utf8');
           const encryptedContent = encrypt(fileContent, password);
-          await writeFile(filePath +".vault", encryptedContent, 'utf8');
-          console.log(`Encrypted ${fileName}`);
+          await writeFile(filePath + ".vault", encryptedContent, 'utf8');
+          console.log(`Encrypted ${filePath}/${fileName}`);
         } else if (mode === 'decrypt' && fileExt === '.vault') {
           const fileContent = await readFile(filePath, 'utf8');
           const decryptedContent = decrypt(fileContent, password);
@@ -68,7 +68,7 @@ async function main() {
   const password = process.env.VAULT_SECRET; // Replace with your encryption password
   assert(password, "No VAULT_SECRET found, please set VAULT_SECRET")
   const rootFolder = 'www'; // Replace with your root folder path
-  
+
   if (['encrypt', 'decrypt'].includes(mode)) {
     await processFiles(rootFolder, mode, password);
   } else {
