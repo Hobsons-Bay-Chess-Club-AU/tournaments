e988dd5790c50895ec77b5d60e397f9f5ba70b87
Truong Nguyen <samuraitruong@hotmail.com>
2024-08-09 21:21:30 +1000

    misc

--- src/update-html.mjs
+++ src/update-html.mjs
@@ -7,7 +7,7 @@ import cheerio from 'cheerio'
 const rootDir = './www';
 
 
-async function fetchAndWriteHtml(filePath , retry=1) {
+async function fetchAndWriteHtml(filePath, retry = 1) {
     // console.log("URL: ", filePath)
 
     var pathUrl = filePath.replace("www/", "")
@@ -16,30 +16,37 @@ async function fetchAndWriteHtml(filePath , retry=1) {
         // console.log("url",pageUrl )
         const response = await axios.get(pageUrl);
         let htmlContent = response.data;
-        
+
         const htmlFilePath = filePath.replace('.php', '.html');
-        if(htmlContent.includes("This site requires Javascript to work")) 
-        {
-            if(retry < 3) {
+        if (htmlContent.includes("This site requires Javascript to work")) {
+            if (retry < 3) {
                 headers.Cookie = await getCookieFromUrl(pageUrl);
                 console.log("New Cookie: ", headers.Cookie)
                 await fetchAndWriteHtml(filePath, retry++)
-                }
+            }
             else
-            console.warn("Invalid coookies, stop overwritten file")
+                console.warn("Invalid coookies, stop overwritten file")
 
         }
         htmlContent = htmlContent.replace(/\.php/g, '.html');
         const $ = cheerio.load(htmlContent);
         $("head").append(
             `  <link href="/hbcc.css" rel="stylesheet">`
-          );
+        );
 
+        $("head").append(
+            `<script src="https://code.jquery.com/jquery-3.7.1.slim.min.js" integrity="sha256-kmHvs0B+OpCW5GVHUNjv9rOmY0IvSIRcf7zGUDTDQM8=" crossorigin="anonymous"></script>`
+        );
+        if (filePath.includes("rankinggroup")) {
+            $("head").append(
+                `<script href="../scripts/ratinggroup.js"></script>`
+            );
+        }
         fs.writeFileSync(htmlFilePath, $.html());
         console.log(`Successful mirror ${pathUrl} => ${htmlFilePath}`);
     } catch (error) {
         console.error(`Error fetching or writing ${filePath}:`, error);
-        if(error.response?.status === 508 && retry<100) {
+        if (error.response?.status === 508 && retry < 100) {
             await fetchAndWriteHtml(filePath, retry++)
         }
     }
@@ -50,25 +57,25 @@ async function traverseDirectory(currentDir) {
     const files = fs.readdirSync(currentDir);
     const promises = [];
     let concurrentCount = 0;
-    
+
     for (const file of files) {
         const filePath = path.join(currentDir, file);
-        
+
         if (fs.statSync(filePath).isDirectory()) {
             promises.push(traverseDirectory(filePath));
         } else if (path.extname(filePath) === '.php') {
             promises.push(fetchAndWriteHtml(filePath));
         }
-        
+
         concurrentCount++;
-        
+
         if (concurrentCount >= 5) {
             await Promise.all(promises);
             promises.length = 0; // Clear the promises array
             concurrentCount = 0;
         }
     }
-    
+
     await Promise.all(promises); // Handle remaining promises
 }
 
---
+++ www/scripts/ratinggroup.js
@@ -0,0 +1,45 @@
+// A $( document ).ready() block.
+$(document).ready(function () {
+    var girls = [];
+    $('table').each(function () {
+        var t = $(this).find('tr:has(.female)').filter(function (index) {
+            return $(this).index() >= 0;
+        });
+        t.each((index, el) => girls.push($(el).clone()));
+        //girls = [...girls, ...t]
+    });
+
+    const existingTable = $("table:first").clone();
+    $('tr', existingTable).remove();
+    existingTable.find('tbody').append(girls)
+    $('tr', existingTable).each((index, element) => {
+        $('td:first', element).text(index + 1)
+
+    });
+    $("div.table-responsive:first").append(`<h5 class="mb-4 mt-4">Girls</h5>`)
+    var el = $(`<div class="table-responsive" id=""></div>`)
+    console.log(existingTable)
+    el.append(existingTable);
+
+    $("div.table-responsive:first").append(el);
+    var prizes = ["Winner", "1st Runner Up", "2nd Runner Up", "3rd Runner Up"]
+    setTimeout(() => {
+        $('table').each(function (index1) {
+            $(this).find('tr').each(function (index) {
+                var td = $('<td class="text-bold text-center"></td>');
+                $(this).append(td)
+                var prizeCount = 4;
+                if (index1 == 3)
+                    prizeCount = 2;
+                if (index > prizeCount) {
+                    $(this).css('background-color', '#333');
+                    $(this).css('color', 'gray');
+                    $(this).css('opacity', '0.25')
+                } else {
+                    td.text(prizes[index])
+                }
+            });
+        });
+    }, 100)
+
+});
