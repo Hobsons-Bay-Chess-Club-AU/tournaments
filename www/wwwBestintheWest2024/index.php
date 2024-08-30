a21d63f7bd81897a597d38aa51f5999f6ce688f2
FTP Sync <ftp@hbcc.com>
2024-08-30 13:13:14 +0000

    Sync FTP changes

--- www/wwwBestintheWest2024/index.php
+++ www/wwwBestintheWest2024/index.php
@@ -20,11 +20,7 @@
    <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">Info</a>
 <ul class="dropdown-menu">
 <li><a class="dropdown-item" href="schedule.php"> Schedule </a></li><li><a class="dropdown-item" href="tourstat.php">Statistics</a></li> </ul></li><li class="nav-item dropdown">   <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">Players</a>
-<ul class="dropdown-menu"><li><a class="dropdown-item" href="index.php">Sorted by Rating</a></li><li><a class="dropdown-item" href="playersname.php">Sorted by Name</a></li></ul></li><li class="nav-item"><a class="nav-link " href="pairs1.php" >Pairings</a>
-</li><li class="nav-item"><a class="nav-link" href="#" >Standings</a>
-</li><li class="nav-item dropdown">   <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">Tables</a>
-<ul class="dropdown-menu"><li><a class="dropdown-item" href="crosstable.php"> Cross table </a></li><li><a class="dropdown-item" href="crosstablescore.php"> Cross table and standings </a></li><li><a class="dropdown-item" href="rankinggroup.php"> Standings by Rtg groups </a></li><li><a class="dropdown-item" href="rankingagegroup.php"> Standings by age groups </a></li><li><a class="dropdown-item" href="rankcat.php"> Standings by title </a></li><li><a class="dropdown-item" href="rankTeams.php"> Rank Teams </a></li><li><a class="dropdown-item" href="summaryFederations.php"> Summary Federations </a></li><li><a class="dropdown-item" href="summaryOrigin.php"> Summary Origin </a></li></ul></li><li class="nav-item dropdown">   <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">Reports</a>
-<ul class="dropdown-menu">          <li><a class="dropdown-item" href="felovar.php">FIDE Rating</a></li>          <li><a class="dropdown-item" href="fideplayercard.php">FIDE Player Card</a></li>           <li><a class="dropdown-item" href="nelovar.php">National rating</a></li>          <li><a class="dropdown-item" href="playercard.php">National Player Card</a></li></ul></li></ul> </div>
+<ul class="dropdown-menu"><li><a class="dropdown-item" href="index.php">Sorted by Rating</a></li><li><a class="dropdown-item" href="playersname.php">Sorted by Name</a></li></ul></li></ul> </div>
 </div>
 </nav>
  <section id="main" class="pt-3 mb-auto">
@@ -117,7 +113,7 @@
 <tr> 
 <td><span class="idn"> 41</span></td><td> <span class="notitle male"> </span> Escobar,David </td><td ><img class="shadow" src="flag/tas.png" alt="TAS"></td><td></td><td>1413</td><td>0</td><td>1413</td> </tr>
 <tr> 
-<td><span class="idn"> 42</span></td><td> <span class="notitle male"> </span> <a href="http://ratings.fide.com/card.phtml?event=3242935" target="X"> Widjaja,Ashton </a></td><td ><img class="shadow" src="flag/vic.png" alt="VIC"></td><td></td><td>1410</td><td>0</td><td>1410</td> </tr>
+<td><span class="idn"> 42</span></td><td> <span class="notitle female"> </span> <a href="http://ratings.fide.com/card.phtml?event=3242935" target="X"> Widjaja,Ashton </a></td><td ><img class="shadow" src="flag/vic.png" alt="VIC"></td><td></td><td>1410</td><td>0</td><td>1410</td> </tr>
 <tr> 
 <td><span class="idn"> 43</span></td><td> <span class="notitle male"> </span> <a href="http://ratings.fide.com/card.phtml?event=33417261" target="X"> Wilford,Harry </a></td><td ><img class="shadow" src="flag/vic.png" alt="VIC"></td><td></td><td>1402</td><td>0</td><td>1402</td> </tr>
 <tr> 
