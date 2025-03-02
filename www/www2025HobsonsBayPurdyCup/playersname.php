fb162e0e5bedabe0cc4a6091865c05a2af2adcc4
FTP Sync <ftp@hbcc.com>
2025-03-02 14:52:18 +0000

    Sync FTP changes

--- www/www2025HobsonsBayPurdyCup/playersname.php
+++ www/www2025HobsonsBayPurdyCup/playersname.php
@@ -40,35 +40,37 @@
   <div class="d-flex flex-column">   <h3 class="mb-4 mt-4">Registered players sorted by name</h3><div class="table-responsive"><table class="table table-striped "> 
 <thead><tr><th>#</th><th>Player</th><th>Fed</th><th>Rtg</th><th>RtgF</th><th>RtgN</th></tr></thead><tbody>
 <tr> 
-<td><span class="idn"> 10</span></td><td> <span class="notitle male"> </span> <a href="http://ratings.fide.com/card.phtml?event=3239683" target="X"> Annapureddy,Dhruthin Reddy </a></td><td ><img class="shadow" src="flag/aus.png" alt="AUS"></td><td>1262</td><td>1509</td><td>1262</td> </tr>
+<td><span class="idn"> 1</span></td><td> <span class="notitle male"> </span> <a href="http://ratings.fide.com/card.phtml?event=3239683" target="X"> Annapureddy,Dhruthin Reddy </a></td><td ><img class="shadow" src="flag/vic.png" alt="VIC"></td><td>1262</td><td>1509</td><td>1262</td> </tr>
 <tr> 
-<td><span class="idn"> 9</span></td><td> <span class="notitle male"> </span> <a href="http://ratings.fide.com/card.phtml?event=3259021" target="X"> Bodke,Krishna </a></td><td ><img class="shadow" src="flag/aus.png" alt="AUS"></td><td>1404</td><td>1696</td><td>1404</td> </tr>
+<td><span class="idn"> 2</span></td><td> <span class="title male"> CM</span> <a href="http://ratings.fide.com/card.phtml?event=3244806" target="X"> Annapureddy,Rheyansh Reddy </a></td><td ><img class="shadow" src="flag/vic.png" alt="VIC"></td><td>2152</td><td>2096</td><td>2152</td> </tr>
 <tr> 
-<td><span class="idn"> 14</span></td><td> <span class="notitle male"> </span> <a href="http://ratings.fide.com/card.phtml?event=3247902" target="X"> Cox,Aiden </a></td><td ><img class="shadow" src="flag/vic.png" alt="VIC"></td><td>0</td><td>0</td><td>0</td> </tr>
+<td><span class="idn"> 3</span></td><td> <span class="notitle male"> </span> <a href="http://ratings.fide.com/card.phtml?event=3259021" target="X"> Bodke,Krishna </a></td><td ><img class="shadow" src="flag/vic.png" alt="VIC"></td><td>1404</td><td>1696</td><td>1404</td> </tr>
 <tr> 
-<td><span class="idn"> 4</span></td><td> <span class="notitle male"> </span> <a href="http://ratings.fide.com/card.phtml?event=3233472" target="X"> Dakic,Dusan </a></td><td ><img class="shadow" src="flag/aus.png" alt="AUS"></td><td>1748</td><td>1765</td><td>1748</td> </tr>
+<td><span class="idn"> 4</span></td><td> <span class="notitle male"> </span> <a href="http://ratings.fide.com/card.phtml?event=3247902" target="X"> Cox,Aiden </a></td><td ><img class="shadow" src="flag/vic.png" alt="VIC"></td><td>0</td><td>0</td><td>0</td> </tr>
 <tr> 
-<td><span class="idn"> 3</span></td><td> <span class="title male"> CM</span> <a href="http://ratings.fide.com/card.phtml?event=3205550" target="X"> Davis,Tony J </a></td><td ><img class="shadow" src="flag/aus.png" alt="AUS"></td><td>1772</td><td>1821</td><td>1772</td> </tr>
+<td><span class="idn"> 5</span></td><td> <span class="notitle male"> </span> <a href="http://ratings.fide.com/card.phtml?event=3233472" target="X"> Dakic,Dusan </a></td><td ><img class="shadow" src="flag/vic.png" alt="VIC"></td><td>1748</td><td>1765</td><td>1748</td> </tr>
 <tr> 
-<td><span class="idn"> 7</span></td><td> <span class="notitle male"> </span> <a href="http://ratings.fide.com/card.phtml?event=3201414" target="X"> Hogan,Steven </a></td><td ><img class="shadow" src="flag/aus.png" alt="AUS"></td><td>1515</td><td>1669</td><td>1515</td> </tr>
+<td><span class="idn"> 6</span></td><td> <span class="title male"> CM</span> <a href="http://ratings.fide.com/card.phtml?event=3205550" target="X"> Davis,Tony J </a></td><td ><img class="shadow" src="flag/vic.png" alt="VIC"></td><td>1772</td><td>1821</td><td>1772</td> </tr>
 <tr> 
-<td><span class="idn"> 2</span></td><td> <span class="notitle male"> </span> <a href="http://ratings.fide.com/card.phtml?event=3204332" target="X"> Hogg,Dean </a></td><td ><img class="shadow" src="flag/aus.png" alt="AUS"></td><td>1928</td><td>1971</td><td>1928</td> </tr>
+<td><span class="idn"> 7</span></td><td> <span class="notitle male"> </span> <a href="http://ratings.fide.com/card.phtml?event=3201414" target="X"> Hogan,Steven </a></td><td ><img class="shadow" src="flag/vic.png" alt="VIC"></td><td>1515</td><td>1669</td><td>1515</td> </tr>
 <tr> 
-<td><span class="idn"> 13</span></td><td> <span class="notitle male"> </span> <a href="http://ratings.fide.com/card.phtml?event=3267857" target="X"> Nicdao,Joseph </a></td><td ><img class="shadow" src="flag/aus.png" alt="AUS"></td><td>599</td><td>1458</td><td>599</td> </tr>
+<td><span class="idn"> 8</span></td><td> <span class="notitle male"> </span> <a href="http://ratings.fide.com/card.phtml?event=3204332" target="X"> Hogg,Dean </a></td><td ><img class="shadow" src="flag/vic.png" alt="VIC"></td><td>1928</td><td>1971</td><td>1928</td> </tr>
 <tr> 
-<td><span class="idn"> 8</span></td><td> <span class="notitle male"> </span> <a href="http://ratings.fide.com/card.phtml?event=3214915" target="X"> Nowak,Ruben </a></td><td ><img class="shadow" src="flag/aus.png" alt="AUS"></td><td>1430</td><td>1590</td><td>1430</td> </tr>
+<td><span class="idn"> 9</span></td><td> <span class="notitle male"> </span> <a href="http://ratings.fide.com/card.phtml?event=3267857" target="X"> Nicdao,Joseph </a></td><td ><img class="shadow" src="flag/vic.png" alt="VIC"></td><td>599</td><td>1458</td><td>599</td> </tr>
 <tr> 
-<td><span class="idn"> 5</span></td><td> <span class="notitle male"> </span> <a href="http://ratings.fide.com/card.phtml?event=3259072" target="X"> Picone,James </a></td><td ><img class="shadow" src="flag/aus.png" alt="AUS"></td><td>1564</td><td>1703</td><td>1564</td> </tr>
+<td><span class="idn"> 10</span></td><td> <span class="notitle male"> </span> <a href="http://ratings.fide.com/card.phtml?event=3214915" target="X"> Nowak,Ruben </a></td><td ><img class="shadow" src="flag/vic.png" alt="VIC"></td><td>1430</td><td>1590</td><td>1430</td> </tr>
 <tr> 
-<td><span class="idn"> 11</span></td><td> <span class="notitle male"> </span> <a href="http://ratings.fide.com/card.phtml?event=3227545" target="X"> Richards,Derek </a></td><td ><img class="shadow" src="flag/aus.png" alt="AUS"></td><td>1219</td><td>1566</td><td>1219</td> </tr>
+<td><span class="idn"> 11</span></td><td> <span class="notitle male"> </span> <a href="http://ratings.fide.com/card.phtml?event=3259072" target="X"> Picone,James </a></td><td ><img class="shadow" src="flag/vic.png" alt="VIC"></td><td>1564</td><td>1703</td><td>1564</td> </tr>
 <tr> 
-<td><span class="idn"> 15</span></td><td> <span class="notitle male"> </span> Srivatsav Yegnavajjula,Aryan </td><td ><img class="shadow" src="flag/vic.png" alt="VIC"></td><td>0</td><td>0</td><td>0</td> </tr>
+<td><span class="idn"> 12</span></td><td> <span class="notitle male"> </span> <a href="http://ratings.fide.com/card.phtml?event=3227545" target="X"> Richards,Derek </a></td><td ><img class="shadow" src="flag/vic.png" alt="VIC"></td><td>1219</td><td>1566</td><td>1219</td> </tr>
 <tr> 
-<td><span class="idn"> 1</span></td><td> <span class="title male"> FM</span> <a href="http://ratings.fide.com/card.phtml?event=3202585" target="X"> Szuveges,Grant J </a></td><td ><img class="shadow" src="flag/aus.png" alt="AUS"></td><td>2079</td><td>2129</td><td>2079</td> </tr>
+<td><span class="idn"> 13</span></td><td> <span class="notitle male"> </span> Srivatsav Yegnavajjula,Aryan </td><td ><img class="shadow" src="flag/vic.png" alt="VIC"></td><td>0</td><td>0</td><td>0</td> </tr>
 <tr> 
-<td><span class="idn"> 6</span></td><td> <span class="title female"> WIM</span> <a href="http://ratings.fide.com/card.phtml?event=3202640" target="X"> Szuveges,Narelle S </a></td><td ><img class="shadow" src="flag/aus.png" alt="AUS"></td><td>1552</td><td>1698</td><td>1552</td> </tr>
+<td><span class="idn"> 14</span></td><td> <span class="title male"> FM</span> <a href="http://ratings.fide.com/card.phtml?event=3202585" target="X"> Szuveges,Grant J </a></td><td ><img class="shadow" src="flag/vic.png" alt="VIC"></td><td>2079</td><td>2129</td><td>2079</td> </tr>
 <tr> 
-<td><span class="idn"> 12</span></td><td> <span class="notitle male"> </span> <a href="http://ratings.fide.com/card.phtml?event=3255689" target="X"> Wilkinson,Greg </a></td><td ><img class="shadow" src="flag/aus.png" alt="AUS"></td><td>1195</td><td>1517</td><td>1195</td> </tr>
+<td><span class="idn"> 15</span></td><td> <span class="title female"> WIM</span> <a href="http://ratings.fide.com/card.phtml?event=3202640" target="X"> Szuveges,Narelle S </a></td><td ><img class="shadow" src="flag/vic.png" alt="VIC"></td><td>1552</td><td>1698</td><td>1552</td> </tr>
+<tr> 
+<td><span class="idn"> 16</span></td><td> <span class="notitle male"> </span> <a href="http://ratings.fide.com/card.phtml?event=3255689" target="X"> Wilkinson,Greg </a></td><td ><img class="shadow" src="flag/vic.png" alt="VIC"></td><td>1195</td><td>1517</td><td>1195</td> </tr>
 </tbody>
 <tfoot><tr><td colspan="7"><strong>Rating legend</strong>: <strong>Rtg</strong>: Rating used for pairing and standings - <strong>RtgF</strong>: Fide Rating - <strong>RtgN</strong>: National Rating</tr></td></tfoot></table></div>
 		  </div>
@@ -80,7 +82,7 @@
             <div class="container">
                 <div class="row">
                     <div class="col-12">
-                        <div class="d-flex flex-column align-items-center text-center text-dark"><a href="http://www.vegachess.com" target="_blank">Generated by Vega</a> All rights reserved Linux version
+                        <div class="d-flex flex-column align-items-center text-center text-dark"><p><a href="http://www.vegachess.com" target="_blank">Generated by Vega</a> All rights reserved</p><p>(Arbiter/Club : Jamie Kenmure - AUS)
   <?php if (file_exists($_SERVER['DOCUMENT_ROOT'].'/siteprivacy.php')) include($_SERVER['DOCUMENT_ROOT'].'/siteprivacy.php'); ?>    </div>  </div>  </div> </div> 
    </footer>
      </div>
