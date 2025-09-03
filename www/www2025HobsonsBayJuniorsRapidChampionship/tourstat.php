3e2eb119172bed569de6cb9d6698ebcfc26d6830
FTP Sync <ftp@hbcc.com>
2025-09-03 03:56:17 +0000

    Sync FTP changes

--- www/www2025HobsonsBayJuniorsRapidChampionship/tourstat.php
+++ www/www2025HobsonsBayJuniorsRapidChampionship/tourstat.php
@@ -49,16 +49,16 @@
 <tr><td >Rounds</td> <td> 7</td> </tr>
 <tr><td >Score game</td> <td> 0-0.5-1</td> </tr>
 <tr><td >Tie break</td> <td>      BH   BH/C1      SB </td> </tr>
-<tr><td >Registered Players</td> <td> 34</td> </tr>
+<tr><td >Registered Players</td> <td> 35</td> </tr>
 <tr><td > Number of Federations</td> <td> 5</td> </tr>
-<tr><td >Average Rating (all) </td> <td> 681</td> </tr>
+<tr><td >Average Rating (all) </td> <td> 675</td> </tr>
 <tr><td >Average Rating (only FIDE rated) </td> <td> 1490</td> </tr>
-<tr ><td > Players from AUS Federation</td> <td> 14</td> </tr>
-<tr><td > Players NOT from AUS Federation</td> <td> 20</td> </tr>
+<tr ><td > Players from AUS Federation</td> <td> 16</td> </tr>
+<tr><td > Players NOT from AUS Federation</td> <td> 19</td> </tr>
 <tr><td > FIDE rated players </td> <td> 14</td> </tr>
-<tr><td > unrated players </td> <td> 20</td> </tr>
+<tr><td > unrated players </td> <td> 21</td> </tr>
 <tr><td > unrated players from AUS Federation</td> <td> 4</td> </tr>
-<tr><td >unrated players NOT from AUS Federation</td> <td> 16</td> </tr>
+<tr><td >unrated players NOT from AUS Federation</td> <td> 17</td> </tr>
 <tr><td > unrated Federations</td> <td> 3</td> </tr>
 <tr><td > FIDE titled players </td> <td> 0</td> </tr>
 <tr><td > GM players </td> <td> 0</td> </tr>
