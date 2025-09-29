e5ad681d81a4895b61e8f9f7e983a7b81ea7bf97
FTP Sync <ftp@hbcc.com>
2025-09-29 03:11:14 +0000

    Sync FTP changes

--- www/www2025HobsonsBayJuniorsRapidChampionship/playercard.php
+++ www/www2025HobsonsBayJuniorsRapidChampionship/playercard.php
@@ -638,6 +638,14 @@
 </tr> </thead><tbody>
 </tbody>
 </table><br>
+<table class="table caption-top table-striped "> 
+<caption>  76   <strong>Wanniarachchie,Senay</strong>, K= 0, Elo=   0, ID=3218680
+<br>
+<a class="anchor" name="76"></a></caption>
+<thead><tr> <th>R</th> <th>C</th> <th >N</th> <th>Cat</th> <th>OPPONENT</th> <th>Rtg</th> <th>res</th> <th>Pts</th>
+</tr> </thead><tbody>
+</tbody>
+</table><br>
 </DIV>
 		  </div>
 		      </div>
