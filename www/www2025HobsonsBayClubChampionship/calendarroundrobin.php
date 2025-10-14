1996bd7c2fec907eb04534bca9e4314d2659462a
FTP Sync <ftp@hbcc.com>
2025-10-14 23:48:42 +0000

    Sync FTP changes

---
+++ www/www2025HobsonsBayClubChampionship/calendarroundrobin.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html lang="en"><head> 
+<meta charset="UTF-8">
+<meta name="description" content="chess tournament">
+<meta name="author" content="www.vegachess.com">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta http-equiv="X-UA-Compatible" content="IE=edge"> 
+<title>2025 Hobsons Bay Club Championship</title>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+<link rel="stylesheet" href="stylenew.css" type="text/css" >
+    </head>
+<body>
+<div class="d-flex flex-column justify-content-between h-100">
+ <nav class="navbar navbar-expand-lg fixed-top">
+                <div class="container">    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul class="navbar-nav me-auto mb-2 mb-lg-0"><li class="nav-item dropdown">
+   <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">Info</a>
+<ul class="dropdown-menu">
+<li><a class="dropdown-item" href="schedule.php"> Schedule </a></li><li><a class="dropdown-item" href="tourstat.php">Statistics</a></li> </ul></li><li class="nav-item dropdown">   <a class="nav-link" href="index.php" >Players</a>
+</li><li class="nav-item"><a class="nav-link " href="pairs1.php" >Pairings</a>
+</li><li class="nav-item"><a class="nav-link" href="#" >Standings</a>
+</li><li class="nav-item dropdown">   <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">Tables</a>
+<ul class="dropdown-menu"><li><a class="dropdown-item" href="sortedcrosstable.php"> Cross table by score </a></li></ul></li><li class="nav-item"><a class="nav-link" href="games.php" >Games</a>
+</li><li class="nav-item dropdown">   <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">Reports</a>
+<ul class="dropdown-menu">          <li><a class="dropdown-item" href="felovar.php">FIDE Rating</a></li>          <li><a class="dropdown-item" href="fideplayercard.php">FIDE Player Card</a></li>           <li><a class="dropdown-item" href="nelovar.php">National rating</a></li>          <li><a class="dropdown-item" href="playercard.php">National Player Card</a></li></ul></li></ul> </div>
+</div>
+</nav>
+ <section id="main" class="pt-3 mb-auto">
+                <div class="container-fluid">
+                    <div class="row">
+                        <div class="d-none d-lg-block col-1 ps-0">
+                            <div class="d-flex flex-column w-full overflow-hidden"> </div> </div> <div class="col-12 col-lg-10">
+  <div class="d-flex flex-column"><h3 class="mb-4 mt-4"> Schedule</h3>
+<h5 class="mb-4 mt-4">Pairing round 1  at 17:30 of 14/10/2025</h5>
+<div class="table-responsive"><table class="table table-striped "> 
+<thead><tr><th>N</th><th>White</th><th>NW</th><th>result</th><th>NB</th><th>Black</th></tr></thead><tbody><tr><td>1</td><td>McIntyre,Julian</td><td>1</td><td> ... </td><td>8</td><td>Hogan,Steven</td></tr><tr><td>2</td><td>Davis,Tony J</td><td>2</td><td> ... </td><td>7</td><td>Nguyen,Anh Kiet</td></tr><tr><td>3</td><td>Krishnan,Anirudh</td><td>3</td><td> ... </td><td>6</td><td>Pasukov,Leo</td></tr><tr><td>4</td><td>Whitford,Matthew</td><td>4</td><td> ... </td><td>5</td><td>Wongso,Renaldo</td></tr></tbody></table></div><hr><h5 class="mb-4 mt-4">Pairing round 2  at 17:30 of 21/10/2025</h5>
+<div class="table-responsive"><table class="table table-striped "> 
+<thead><tr><th>N</th><th>White</th><th>NW</th><th>result</th><th>NB</th><th>Black</th></tr></thead><tbody><tr><td>1</td><td>Hogan,Steven</td><td>8</td><td>...</td><td>5</td><td>Wongso,Renaldo</td></tr><tr><td>2</td><td>Pasukov,Leo</td><td>6</td><td>...</td><td>4</td><td>Whitford,Matthew</td></tr><tr><td>3</td><td>Nguyen,Anh Kiet</td><td>7</td><td>...</td><td>3</td><td>Krishnan,Anirudh</td></tr><tr><td>4</td><td>McIntyre,Julian</td><td>1</td><td>...</td><td>2</td><td>Davis,Tony J</td></tr></tbody></table></div><hr><h5 class="mb-4 mt-4">Pairing round 3  at 17:30 of 28/10/2025</h5>
+<div class="table-responsive"><table class="table table-striped "> 
+<thead><tr><th>N</th><th>White</th><th>NW</th><th>result</th><th>NB</th><th>Black</th></tr></thead><tbody><tr><td>1</td><td>Davis,Tony J</td><td>2</td><td>...</td><td>8</td><td>Hogan,Steven</td></tr><tr><td>2</td><td>Krishnan,Anirudh</td><td>3</td><td>...</td><td>1</td><td>McIntyre,Julian</td></tr><tr><td>3</td><td>Whitford,Matthew</td><td>4</td><td>...</td><td>7</td><td>Nguyen,Anh Kiet</td></tr><tr><td>4</td><td>Wongso,Renaldo</td><td>5</td><td>...</td><td>6</td><td>Pasukov,Leo</td></tr></tbody></table></div><hr><h5 class="mb-4 mt-4">Pairing round 4  at 17:30 of 11/11/2025</h5>
+<div class="table-responsive"><table class="table table-striped "> 
+<thead><tr><th>N</th><th>White</th><th>NW</th><th>result</th><th>NB</th><th>Black</th></tr></thead><tbody><tr><td>1</td><td>Hogan,Steven</td><td>8</td><td>...</td><td>6</td><td>Pasukov,Leo</td></tr><tr><td>2</td><td>Nguyen,Anh Kiet</td><td>7</td><td>...</td><td>5</td><td>Wongso,Renaldo</td></tr><tr><td>3</td><td>McIntyre,Julian</td><td>1</td><td>...</td><td>4</td><td>Whitford,Matthew</td></tr><tr><td>4</td><td>Davis,Tony J</td><td>2</td><td>...</td><td>3</td><td>Krishnan,Anirudh</td></tr></tbody></table></div><hr><h5 class="mb-4 mt-4">Pairing round 5  at 17:30 of 18/11/2025</h5>
+<div class="table-responsive"><table class="table table-striped "> 
+<thead><tr><th>N</th><th>White</th><th>NW</th><th>result</th><th>NB</th><th>Black</th></tr></thead><tbody><tr><td>1</td><td>Krishnan,Anirudh</td><td>3</td><td>...</td><td>8</td><td>Hogan,Steven</td></tr><tr><td>2</td><td>Whitford,Matthew</td><td>4</td><td>...</td><td>2</td><td>Davis,Tony J</td></tr><tr><td>3</td><td>Wongso,Renaldo</td><td>5</td><td>...</td><td>1</td><td>McIntyre,Julian</td></tr><tr><td>4</td><td>Pasukov,Leo</td><td>6</td><td>...</td><td>7</td><td>Nguyen,Anh Kiet</td></tr></tbody></table></div><hr><h5 class="mb-4 mt-4">Pairing round 6  at 17:30 of 25/11/2025</h5>
+<div class="table-responsive"><table class="table table-striped "> 
+<thead><tr><th>N</th><th>White</th><th>NW</th><th>result</th><th>NB</th><th>Black</th></tr></thead><tbody><tr><td>1</td><td>Hogan,Steven</td><td>8</td><td>...</td><td>7</td><td>Nguyen,Anh Kiet</td></tr><tr><td>2</td><td>McIntyre,Julian</td><td>1</td><td>...</td><td>6</td><td>Pasukov,Leo</td></tr><tr><td>3</td><td>Davis,Tony J</td><td>2</td><td>...</td><td>5</td><td>Wongso,Renaldo</td></tr><tr><td>4</td><td>Krishnan,Anirudh</td><td>3</td><td>...</td><td>4</td><td>Whitford,Matthew</td></tr></tbody></table></div><hr><h5 class="mb-4 mt-4">Pairing round 7  at 17:30 of 02/12/2025</h5>
+<div class="table-responsive"><table class="table table-striped "> 
+<thead><tr><th>N</th><th>White</th><th>NW</th><th>result</th><th>NB</th><th>Black</th></tr></thead><tbody><tr><td>1</td><td>Whitford,Matthew</td><td>4</td><td>...</td><td>8</td><td>Hogan,Steven</td></tr><tr><td>2</td><td>Wongso,Renaldo</td><td>5</td><td>...</td><td>3</td><td>Krishnan,Anirudh</td></tr><tr><td>3</td><td>Pasukov,Leo</td><td>6</td><td>...</td><td>2</td><td>Davis,Tony J</td></tr><tr><td>4</td><td>Nguyen,Anh Kiet</td><td>7</td><td>...</td><td>1</td><td>McIntyre,Julian</td></tr></tbody></table></div><hr>		  </div>
+		      </div>
+         <div class="d-none d-lg-block col-1 pe-0">
+           <div class="d-flex flex-column w-full overflow-hidden">
+   </div> </div>  </div>  </div> </section>
+    <footer id="footer" class="bg-light py-5 mt-5 border-bottom border-light-subtle border-5">
+            <div class="container">
+                <div class="row">
+                    <div class="col-12">
+                        <div class="d-flex flex-column align-items-center text-center text-dark"><p><a href="http://www.vegachess.com" target="_blank">Generated by Vega</a> All rights reserved</p><p>(Arbiter/Club : Hobsons Bay Chess Club - AUS)
+  <?php if (file_exists($_SERVER['DOCUMENT_ROOT'].'/siteprivacy.php')) include($_SERVER['DOCUMENT_ROOT'].'/siteprivacy.php'); ?>    </div>  </div>  </div> </div> 
+   </footer>
+     </div>
+       <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script></body>
+</html>
