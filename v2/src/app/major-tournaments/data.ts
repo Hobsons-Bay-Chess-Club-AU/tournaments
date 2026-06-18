1e9cbc7f4a35c55a37dd73e2c5b367a05f3ec3ce
Truong Nguyen <samuraitruong@hotmail.com>
2026-06-18 20:41:40 +1000

    Add Victorian Junior Rapid Championship 2026 to major tournaments

--- v2/src/app/major-tournaments/data.ts
+++ v2/src/app/major-tournaments/data.ts
@@ -6,5 +6,14 @@ export const majorTournaments = [
         start_date: "2025-07-17",
         end_date: "2025-07-25",
     },
+
+    {
+        name: "Victorian Junior Rapid Championship 2026",
+        slug: "2026-victorian-junior-rapid-championship",
+        url: "https://www.vegaresults.com/vr/5339",
+        start_date: "2026-10-03",
+        end_date: "2026-10-04",
+    },
+
     // Add more tournaments here
 ];
