bec542d49a81dcbda6b6694b5febd8f3a27721ce
FTP Sync <ftp@hbcc.com>
2026-08-01 03:54:17 +0000

    Sync FTP changes

--- www/www2026BITW/playercard.php
+++ www/www2026BITW/playercard.php
@@ -1101,6 +1101,30 @@
 </tr> </thead><tbody>
 </tbody>
 </table><br>
+<table class="table caption-top table-striped "> 
+<caption> 135   <strong>Dissanayake,Naveen  </strong>, K= 0, Elo=1210, ID=3198774
+<br>
+<a class="anchor" name="135"></a></caption>
+<thead><tr> <th>R</th> <th>C</th> <th >N</th> <th>Cat</th> <th>OPPONENT</th> <th>Rtg</th> <th>res</th> <th>Pts</th>
+</tr> </thead><tbody>
+</tbody>
+</table><br>
+<table class="table caption-top table-striped "> 
+<caption> 136   <strong>Dissanayake,Vidushi </strong>, K= 0, Elo=1627, ID=3193336
+<br>
+<a class="anchor" name="136"></a></caption>
+<thead><tr> <th>R</th> <th>C</th> <th >N</th> <th>Cat</th> <th>OPPONENT</th> <th>Rtg</th> <th>res</th> <th>Pts</th>
+</tr> </thead><tbody>
+</tbody>
+</table><br>
+<table class="table caption-top table-striped "> 
+<caption> 137   <strong>Patel,Jinay         </strong>, K= 0, Elo=   0, ID=3223381
+<br>
+<a class="anchor" name="137"></a></caption>
+<thead><tr> <th>R</th> <th>C</th> <th >N</th> <th>Cat</th> <th>OPPONENT</th> <th>Rtg</th> <th>res</th> <th>Pts</th>
+</tr> </thead><tbody>
+</tbody>
+</table><br>
 </DIV>
 		  </div>
 		      </div>
